4bea0728dc75fa3a203624fe473a04c41be94aff
PlatenPrime <proshta@gmail.com>
2022-10-09 14:12:06 +0300

    row

--- client/src/pages/Row/AddRowPage.jsx
+++ client/src/pages/Row/AddRowPage.jsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { toast } from 'react-toastify';
+
+import { createRow } from "../../redux/features/row/rowSlice";
+import { checkIsAuth } from '../../redux/features/auth/authSlice';
+
+
+
+
+
+
+
+
+
 
 const AddRowPage = () => {
 	return (
